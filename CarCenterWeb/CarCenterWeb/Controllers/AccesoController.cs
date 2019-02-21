cb03ec48ff8b9442463385d3fafe1fa889e709fb
yencyachipiz <khurtado2042@gmail.com>
2019-02-20 20:58:20 -0500

    implementar login

---
+++ CarCenterWeb/CarCenterWeb/Controllers/AccesoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarCenterWeb.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
