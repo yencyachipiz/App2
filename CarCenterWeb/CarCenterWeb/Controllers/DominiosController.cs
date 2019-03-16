8eecfd91e9e27ea39317e85be5c0377e3b20e903
Soft2-02 <khurtdo2042@gmail.com>
2019-03-16 11:44:08 -0500

    clase

--- CarCenterWeb/CarCenterWeb/Controllers/DominiosController.cs
+++ CarCenterWeb/CarCenterWeb/Controllers/DominiosController.cs
@@ -33,9 +33,67 @@ namespace CarCenterWeb.Controllers
         }
         //metedo que  llama al formulario  de insercion
         [HttpGet]
-        public ActionResult Add() {
+        public ActionResult Add()
+        {
             return View();
         }
-    }
 
+        //metodo que va a guardar en la BD
+        [HttpPost]
+        public ActionResult Add(DominiosDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (carcenterEntities db=new carcenterEntities())
+            {
+                DOMINIOS oDominio = new DOMINIOS();
+                oDominio.TIPO_DOMINIO = model.TipoDominio;
+                oDominio.ID_DOMINIO = model.VlrDominio;
+                oDominio.VLR_DOMINIO = model.VlrDominio;
+
+                db.DOMINIOS.Add(oDominio);
+                db.SaveChanges();
+
+            }
+            return Redirect(Url.Content("~Dominios/"));
+            
+        }
+
+        public ActionResult Edit(string tipoDominio, string idDominio)
+        {
+            DominiosDTO model = new DominiosDTO();
+            using (carcenterEntities db= new carcenterEntities())
+            {
+                DOMINIOS oDominio = db DOMINIOS.Find(tipoDominio, idDominio);
+                model.TipoDominio = oDominio.TIPO_DOMINIO;
+                model.IdDominio = oDominio.ID_DOMINIO;
+                model.VlrDominio = oDominio.VLR_DOMINIO;
+            }
+            return View(model);
+        }
+        public ActionResult Edit(DominiosDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            //editar registrar en la BD
+
+            using(carcenterEntities db= new carcenterEntities())
+            {
+                DOMINIOS oDominio = db.DOMINIOS.Find(model.TipoDominio, model.IdDominio);
+                oDominio.TIPO_DOMINIO = model.TipoDominio;
+                oDominio.ID_DOMINIO = model.TipoDominio;
+                oDominio.VLR_DOMINIO = model.VlrDominio;
+
+                db.Entry(oDominio).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Redirect(Url.Content("~Dominio"));
+
+        }
+    }
 }
